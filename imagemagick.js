2efd01c69614a0888175af2d18a5c16b57d6aa73
Samer Eberlin <samer.eberlin@maersk.com>
2022-03-31 15:57:28 +0200

    Move readline into the library.

--- imagemagick.js
+++ imagemagick.js
@@ -2,10 +2,19 @@
 
 const lib = require('./lib.js');
 
-if (process.argv.length != 3) {
-	console.log(`Usage: ${lib.getAppName()} . | directory | file`);
-	process.exit(1);
+console.log(lib.COLORS.YELLOWB + '====> Testing colors...' + lib.COLORS.RESET);
+
+async function main() {
+	if (process.argv.length != 3) {
+		console.log(`Usage: ${lib.getAppName()} . | directory | file`);
+		process.exit(1);
+	}
+
+	const answer = await lib.prompt('extensions? ');
+	console.log('====> answer:', answer);
+
+	console.log('====> ', lib.findFiles(process.cwd(), /.*js$/i));
+	process.exit(0);
 }
 
-
-console.log("====> ", lib.findFiles(process.cwd(), /.*txt$/i));
+main();
--- lib.js
+++ lib.js
@@ -1,14 +1,18 @@
 const fs = require('fs');
 const path = require('path');
+const rl = require('readline').createInterface({input: process.stdin, output: process.stdout});
+
+const COLORS = {
+	RESET: '\x1b[0m',
+	BRIGHT: '\x1b[1m',
+	GREENB: '\x1b[1,32m',
+	REDB: '\x1b[1,31m',
+	YELLOWB: '\x1b[1,33m',
+}
 
 module.exports = {
-	COLORS: {
-		RESET: '\x1b[0m',
-		BRIGHT: '\x1b[1m',
-		GREENB: '\x1b[1,32m',
-		REDB: '\x1b[1,31m',
-		YELLOWB: '\x1b[1,33m',
-	},
+	COLORS,
+
 	findFiles: function (findPath, regexpFilter) {
 		const findRecursively = function (currentPath, arrayOfFiles = []) {
 			const files = fs.readdirSync(currentPath);
@@ -24,7 +28,14 @@ module.exports = {
 		};
 		return findRecursively(findPath);
 	},
+
 	getAppName: function () {
 		return path.basename(process.argv[1]);
+	},
+
+	prompt: function (text) {
+		return new Promise((resolve) => {
+			rl.question(`${COLORS.YELLOWB}${text}${COLORS.RESET}`, resolve);
+		});
 	},
 };
