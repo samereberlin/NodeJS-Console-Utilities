03847d6cf3c92de0192ca100d7a86170458ffa52
Samer Eberlin <samer.eberlin@maersk.com>
2022-08-05 21:40:26 +0200

    Update script based on ffmpeg.sh.

--- imagemagick.js
+++ imagemagick.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
 const lib = require('./lib.js');
 const path = require('path');
 
@@ -10,18 +11,37 @@ async function main() {
 	if (dirs.length) {
 		const findRegExp = new RegExp(await lib.prompt('Regexp filter for directories?', '.*\\.(jpe?g|png)$'), 'i');
 		for (const dir of dirs) {
-			files.push(...(await lib.findFiles(dir, findRegExp)));
+			files.push(...lib.findFiles(dir, findRegExp));
 		}
 	}
 
 	const errorList = [];
 	const options = await lib.prompt('Convert options?', '-resize 3840x\\> -quality 85');
 	const suffix = await lib.prompt('Output file suffix?', '_out');
-	const discard = /^[^n]*$/i.test(await lib.prompt('Discard input files? [Y/n]:', 'Y'));
+
+	let discard = '';
+	do {
+		discard = await lib.prompt('Discard input files? [Y/n]:', 'Y');
+	} while (!/^[ny]$/i.test(discard));
+	discard = discard.toLowerCase() === 'y';
 
 	for (const [index, file] of files.entries()) {
+		lib.printTextColor('--------------------------------------------------------------------------------');
 		lib.printMessage(`converting ${index + 1} of ${files.length} files: ${file}`);
-		const output = file.substring(0, file.length - path.extname(file).length) + suffix + '.jpg';
+
+		const extNameLength = path.extname(file).length;
+		if (file.substring(file.length - extNameLength - suffix.length, file.length - extNameLength) === suffix) {
+			lib.printError(`${file} (input file already suffixed)`);
+			errorList.push(`${file} (input file already suffixed)`);
+			continue;
+		}
+
+		const output = `${file.substring(0, file.length - extNameLength)}${suffix}.mp4`;
+		if (fs.existsSync(output)) {
+			lib.printError(`${file} (output file already exists)`);
+			errorList.push(`${file} (output file already exists)`);
+			continue;
+		}
 
 		try {
 			await lib.execCommand(`convert ${file} ${options} ${output}`);
@@ -29,13 +49,15 @@ async function main() {
 				await lib.execCommand(`rm -f ${file}`);
 			}
 		} catch (error) {
+			lib.printError(`While converting file: ${file}`);
 			errorList.push(file);
 		}
 	}
 
 	if (errorList.length) {
+		lib.printTextColor('--------------------------------------------------------------------------------');
 		lib.printError('While converting files:');
-		errorList.forEach((errorFile) => lib.printError('', `\t${errorFile}`));
+		errorList.forEach((errorItem) => lib.printError('', `\t${errorItem}`));
 	}
 	process.exit(0);
 }
